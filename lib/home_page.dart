805499b02a8832b00a330aba6d79211b8d5d27ef
shakiz <activities335@gmail.com>
2022-01-28 10:28:57 +0600

    range and home updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_top_widgets/adaptive_widgets/adaptive_widgets.dart';
 import 'package:flutter_top_widgets/choice_chip_widget/choice_chip_widget.dart';
+import 'package:flutter_top_widgets/date_time_widget/date_time_widget.dart';
 import 'package:flutter_top_widgets/expansion_tile_and_wrap/expansion_tile_and_wrap.dart';
+import 'package:flutter_top_widgets/page_view_widget/page_view_widget.dart';
 import 'package:flutter_top_widgets/sliver_app_bar_widget/sliver_app_bar_widget.dart';
 import 'package:flutter_top_widgets/stepper_manager/stepper_manager.dart';
+import 'package:flutter_top_widgets/table_widget/table_widget.dart';
 
+import 'adaptive_and_range_widgets/adaptive_and_range_widgets.dart';
 import 'appbar_search_delegate/custom_appbar_search_delegate.dart';
 
 class HomePage extends StatefulWidget {
@@ -74,14 +77,14 @@ class _HomePageState extends State<HomePage> {
                 )),
               ),
               onPressed: () async {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const AdaptiveWidgets()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const AdaptiveAndRangeWidgets()));
               },
               //LOGIN //Drawer Main
               child: const Padding(
                 padding: EdgeInsets.all(8),
                 child: Center(
                   child: Text(
-                    "Adaptive Widgets",
+                    "Adaptive and Range Widgets",
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                   ),
@@ -151,6 +154,75 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                   child: Text(
                     "Expansion Tile and Wrap",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                )),
+              ),
+              onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const DateTimeWidget()));
+              },
+              //LOGIN //Drawer Main
+              child: const Padding(
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Text(
+                    "Date Time Widget",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                )),
+              ),
+              onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const PageViewWidget()));
+              },
+              //LOGIN //Drawer Main
+              child: const Padding(
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Text(
+                    "Page View Slider Widget",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                )),
+              ),
+              onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const TableWidget()));
+              },
+              //LOGIN //Drawer Main
+              child: const Padding(
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Text(
+                    "Table Widget",
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                   ),
